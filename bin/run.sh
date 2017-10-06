4a7ccadeb584cb4d0e917328509cc9feccc0cf86
Jan Klemkow <j.klemkow@wemelug.de>
2017-10-06 21:15:04 +0200

    make install and power commands optional

--- bin/run.sh
+++ bin/run.sh
@@ -4,21 +4,29 @@ action="$SSH_ORIGINAL_COMMAND"
 setenv="/usr/local/bin/envdir /home/$USER/env"
 PATH="/home/test/bin:$PATH"
 
+function no_command {
+	echo "command is not configured for $USER"
+	exit 1
+}
+
 case "$action" in
 	""|"console")
 		exec /usr/local/bin/console "$USER"
 		;;
 	"on"|"off"|"cycle")
+		if ! test -f /home/$USER/env/powerdevice; then no_command; fi
 		if $setenv checklock.sh; then
 			exec $setenv power.sh $action
 		fi
 		;;
 	"setup"|"install")
+		if ! test -f /home/$USER/env/install; then no_command; fi
 		if $setenv checklock.sh; then
 			exec $setenv install.sh
 		fi
 		;;
 	"upgrade")
+		if ! test -f /home/$USER/env/install; then no_command; fi
 		if $setenv checklock.sh; then
 			exec $setenv upgrade.sh
 		fi
@@ -36,11 +44,15 @@ case "$action" in
 		;;
 	"help")
 		echo "        - console"
-		echo "on      - power on"
-		echo "off     - power off"
-		echo "cycle   - power cycle"
-		echo "install - install current snapshot"
-		echo "upgrade - upgrade to current snapshot"
+		if test -f /home/$USER/env/powerdevice; then
+			echo "on      - power on"
+			echo "off     - power off"
+			echo "cycle   - power cycle"
+		fi
+		if test -f /home/$USER/env/install; then
+			echo "install - install current snapshot"
+			echo "upgrade - upgrade to current snapshot"
+		fi
 		echo "lock    - lock access to machine"
 		echo "free    - free access to machine"
 		echo "help    - print this message"
