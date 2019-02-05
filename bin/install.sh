86bc4af3c4312905a332df52aec50598b0e10944
Moritz Buhl <gh@moritzbuhl.de>
2019-02-06 00:51:07 +0100

    Refactor install

--- bin/install.sh
+++ bin/install.sh
@@ -39,6 +39,9 @@ mk_install_conf() {
 		Password for user = ${userpw}
 		Public ssh key for user = ${sshkey}
 		Allow root ssh login = yes
+$(if [ -n "${disk:-}" ]; then
+	echo "Which disk is the root disk = ${disk}";
+fi)
 		What timezone are you in = Europe/Berlin
 		Location of sets = http
 		Server = ${setserver}
@@ -58,49 +61,11 @@ mk_install_conf() {
 #		Unverified sets: site${version}.tgz. Continue without verification = yes
 }
 
-mk_install_conf_arm() {
-	rm -f ${1}
-
-	cat > ${1} <<- EOF
-		System hostname = ${machine}
-		DNS domain name = obsd-lab.genua.de
-		Password for root = ${rootpw}
-		Public ssh key for root = ${sshkey}
-		Network interfaces = ${interface}
-		IPv4 address for ${interface} = ${ipaddr}
-		Default IPv4 route = 10.0.1.1
-		Setup a user = user
-		Password for user = ${userpw}
-		Public ssh key for user = ${sshkey}
-		Allow root ssh login = yes
-$(if [ -s "${disk:-}" ]; then
-	echo "Which disk is the root disk = ${disk}";
-fi)
-		What timezone are you in = Europe/Berlin
-		Location of sets = http
-		Server = ${setserver}
-		Server directory = pub/OpenBSD/${release}/${arch}
-		Use http instead = yes
-		Set name(s) = done
-		Location of sets = http
-		Server = ${siteserver}
-		Server directory = site
-		Use http instead = yes
-		INSTALL.${arch} not found. Use sets found here anyway = yes
-		Set name(s) = done
-		Continue without verification = yes
-	EOF
-}
-
 set_dhcpd_conf() {
 
 	set -eux
 
 	file="auto_install";
-
-	if [ "$arch" = "armv7" ]; then
-		file="auto_install";
-	fi
 
 	case "$1" in
 		"on")
@@ -136,55 +101,29 @@ on_exit() {
 trap on_exit EXIT
 
 tftp_dir="/var/spool/tftp/${machine}"
-
 if [ "$arch" = "armv7" ]; then
-
-	mkdir -p ${tftp_dir}
-
-	mkdir -p /var/www/htdocs/${machine}
-	mk_install_conf_arm /var/www/htdocs/${hwaddr}-install.conf
-
-	ftp -o ${tftp_dir}/bsd http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/bsd.rd
-	ftp -o ${tftp_dir}/auto_install http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/BOOTARM.EFI
-
+	netboot="BOOTARM.EFI"
 elif [ "$arch" = "arm64" ]; then
-
-	mkdir -p ${tftp_dir}
-
-	mkdir -p /var/www/htdocs/${machine}
-	mk_install_conf_arm /var/www/htdocs/${hwaddr}-install.conf
-
-	ftp -o ${tftp_dir}/bsd http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/bsd.rd
-	ftp -o ${tftp_dir}/auto_install http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/BOOTAA64.EFI
-
+	netboot="BOOTAA64.EFI"
 elif [ "$arch" = "sparc64" ]; then
+	netboot="ofwboot.net"
+else # default x86
+	netboot="pxeboot"
+fi
 
-	mkdir -p ${tftp_dir}
+mkdir -p /var/www/htdocs/${machine}
+mk_install_conf /var/www/htdocs/${hwaddr}-install.conf
 
-	mkdir -p /var/www/htdocs/${machine}
-	mk_install_conf_arm /var/www/htdocs/${hwaddr}-install.conf
+mkdir -p ${tftp_dir}
 
-	ftp -o ${tftp_dir}/bsd http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/bsd.rd
-	ftp -o ${tftp_dir}/ofwboot.net http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/ofwboot.net
+ftp -o ${tftp_dir}/auto_install http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/${netboot}
+ftp -o ${tftp_dir}/bsd http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/bsd.rd
 
-else
-	#
-	# x86 default
-	#
+if [ -s "${tftp_dir}/${target:-invalid}" ]; then
+	cp "${tftp_dir}/${target:-invalid}" "${tftp_dir}/auto_install"
+fi
 
-	# get current bsd.rd and pxeboot file
-	rm -f ${tftp_dir}/bsd.new
-	ftp -o ${tftp_dir}/bsd.new http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/bsd.rd
-	mv ${tftp_dir}/bsd.new ${tftp_dir}/bsd
-	ftp -o ${tftp_dir}/auto_install http://[2001:a60:91df:c000::16]/pub/OpenBSD/${release}/${arch}/pxeboot
-
-	if [ -s "${tftp_dir}/${target:-invalid}" ]; then
-		cp "${tftp_dir}/${target:-invalid}" "${tftp_dir}/auto_install"
-	fi
-
-	mkdir -p /var/www/htdocs/${machine}
-	mk_install_conf /var/www/htdocs/${hwaddr}-install.conf
-
+if [ "$arch" = "i386" -o "$arch" = "amd64" ]; then
 	# generate random.seed file
 	mkdir -p ${tftp_dir}/etc -m 775
 	tmprand=`mktemp -p ${tftp_dir}/etc random.seed.XXXXXXXXXX`
