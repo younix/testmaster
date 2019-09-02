ade6464e6f7e927905c490749fd0778d6d20cc8b
Moritz Buhl <gh@moritzbuhl.de>
2019-09-03 00:46:51 +0200

    add script to gather machine info

---
+++ bin/hw_info.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+set -eux
+
+host=$1
+dmesg=$(mktemp)
+sysctl=$(mktemp)
+ifconfig=$(mktemp)
+usbdevs=$(mktemp)
+pcidump=$(mktemp)
+
+ssh root@$host 'cat /var/run/dmesg.boot' > $dmesg
+ssh root@$host sysctl > $sysctl
+ssh root@$host ifconfig > $ifconfig
+ssh root@$host 'usbdevs -vv' > $usbdevs
+ssh root@$host 'pcidump -v' > $pcidump
+
+# extract devices em0 etc from dmesg
+#cat $out | grep -e '^[a-z][a-z]*[0-9] ' | cut -d' ' -f1 | sort | uniq | less
+#cat $out | grep -e '^[a-z][a-z]*[0-9] ' | sort | uniq | less
+
+cat > "./$host-info.html" <<- EOF
+<!doctype html>
+<html lang="en-US">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+pre {
+	white-space: pre-wrap;
+	width: 80ch;
+	word-wrap: anywhere;
+}
+	</style>
+        <title>$host info</title>
+</head>
+<body>
+
+<h1><em>Open</em>BSDLab - $host info</h1>
+
+<nav>
+<a href="index.html">Home</a>
+<a href="otx-config.html">otx config</a>
+<a href="newuser.html">new user</a>
+<a href="ssh_config.html">ssh_config</a>
+<a href="faq.html">faq</a>
+<hr></nav>
+
+<p>Automatically gathered information on $host.<br>
+<em>last updated: $(date "+%Y-%m-%d %H:%M:%S")</em></p>
+
+<nav>
+<a href="#dmesg">dmesg</a>
+<a href="#sysctl">sysctl</a>
+<a href="#ifconfig">ifconfig</a>
+<a href="#usbdevs">usbdevs</a>
+<a href="#pcidump">pcidump</a>
+</nav>
+
+<h2 id="dmesg">dmesg</h2>
+<pre>$(cat $dmesg)</pre>
+
+<h2 id="sysctl">sysctl</h2>
+<pre>$(cat $sysctl)</pre>
+
+<h2 id="ifconfig">ifconfig</h2>
+<pre>$(cat $ifconfig)</pre>
+
+<h2 id="usbdevs">usbdevs</h2>
+<pre>$(cat $usbdevs)</pre>
+
+<h2 id="pcidump">pcidump</h2>
+<pre>$(cat $pcidump)</pre>
+
+<footer><hr><a href="https://www.genua.de/impressum.html">Impressum</a></footer>
+
+</body>
+</html>
+EOF
