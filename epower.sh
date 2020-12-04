18a9c3c87ef762e0844a97066b1b6d5711463e14
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-12-04 21:57:42 +0100

    Shorten long line in epower.

--- epower.sh
+++ epower.sh
@@ -26,4 +26,5 @@ case "$action" in
 	;;
 esac
 
-curl -s -u admin:admin -d "P${port}=${action}" -X POST http://10.0.1.17/config/home_f.html
+curl -s -u admin:admin -d "P${port}=${action}" -X POST \
+    http://10.0.1.17/config/home_f.html
