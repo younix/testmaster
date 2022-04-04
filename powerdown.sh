e7bbb7a5fc303aee185fc69d31c90c3a17d8125d
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-04-04 18:12:39 +0200

    Wait for syncing disks before power off machine.

--- powerdown.sh
+++ powerdown.sh
@@ -6,4 +6,13 @@ PATH="/home/test/bin:$PATH"
 
 echo "power down machine $machine"
 
-ssh root@${ipaddr} shutdown -p now power down by testmaster || power.sh off
+if ssh root@${ipaddr} shutdown -p now power down by testmaster; then
+	if ! syncingdisks.expect; then
+		echo "no syncing disks on machine $machine"
+	else
+		# allow graceful power down
+		sleep 5
+	fi
+fi
+
+power.sh off
