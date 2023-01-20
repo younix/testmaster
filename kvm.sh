d35e59e4801b4d239e2314cb20c7c292f4eb0464
Jan Klemkow <j.klemkow@wemelug.de>
2023-01-20 19:29:39 +0100

    add kvm.sh to powercycle kvm vms

---
+++ kvm.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+port=$1
+status=$2
+
+if [ ${status} -lt 0 -o ${status} -gt 1 ]; then
+	echo "unkown status: ${status}"
+	exit 1
+fi
+
+case "${status}" in
+	"0")
+	echo "power off, machine ${machine:--}"
+	virsh destroy ${machine} 
+	;;
+
+	"1")
+	echo "power on, machine ${machine:--}"
+	virsh start ${machine} 
+	;;
+esac
