ec4f5a78727d3c964f10dba16b952992603a5a48
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-07-25 01:21:02 +0200

    Do key rollout also for Linux and FreeBSD machines to access console.

--- keyrollout.sh
+++ keyrollout.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-for m in /home/ot*/users /home/ft*/users /home/virt/users; do
+# FreeBSD, Linux, OpenBSD Test and virt KVM server
+for m in /home/[flo]t*/users /home/virt/users; do
 	authfile="$(dirname "$m")/.ssh/authorized_keys"
 	echo -n "renew $authfile"
 	rm -f "$authfile"
